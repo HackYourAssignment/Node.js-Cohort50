2ed79f39b76742e4544ed09f3ab40f8fa349d08b
Alex P <a117a117a@gmail.com>
2024-11-25 16:43:12 +0100

    updated server.js

--- assignments/hackyourtemperature/server.js
+++ assignments/hackyourtemperature/server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { httpStatus, messages } from './httpStatusCodes.js';
+import { sendResponse } from './responseUtils.js';
+import { errorHandler } from './errorHandler.js';
 
 const app = express();
 const PORT = 3000;
@@ -6,13 +9,22 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('hello from backend to frontend!');
+    sendResponse(res, httpStatus.OK, null, 'hello from backend to frontend!');
 });
 
 app.post('/weather', (req, res) => {
     const cityName = req.body.cityName;
-    res.json({ message: `You submitted: ${cityName}` });
+    
+    if (!cityName) {
+        const error = new Error(messages.BAD_REQUEST);
+        error.status = httpStatus.BAD_REQUEST;
+        return next(error);
+    }
+
+    sendResponse(res, httpStatus.OK, { cityName }, `You submitted: ${cityName}`);
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
