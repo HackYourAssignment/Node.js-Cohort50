34254f2249a66677eada4c1554798bfd043d4194
Alex P <a117a117a@gmail.com>
2024-11-25 15:43:18 +0100

    added first POST request

--- assignments/hackyourtemperature/server.js
+++ assignments/hackyourtemperature/server.js
@@ -3,8 +3,15 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
+});
+
+app.post('/weather', (req, res) => {
+    const cityName = req.body.cityName;
+    res.json({ message: `You submitted: ${cityName}` });
 });
 
 app.listen(PORT, () => {
