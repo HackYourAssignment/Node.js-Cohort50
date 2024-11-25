69c46b18c9a3587b932c737018087988eb39da90
Alex P <a117a117a@gmail.com>
2024-11-25 16:36:20 +0100

    added status codes consts

---
+++ assignments/hackyourtemperature/httpStatusCodes.js
@@ -0,0 +1,13 @@
+export const httpStatus = {
+    OK: 200,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+};
+  
+export const messages = {
+    BAD_REQUEST: 'Bad request: Missing required fields',
+    NOT_FOUND: 'Resource not found',
+    SERVER_ERROR: 'Internal Server Error',
+    SUCCESS: 'Request successful',
+};
