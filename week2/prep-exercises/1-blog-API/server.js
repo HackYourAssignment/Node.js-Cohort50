c436276f5a3f0808e4ad595c6b1090ed2549b728
Abdul Kader <abdulkader@tikapp.link>
2024-11-30 23:36:20 +0100

    create read, edit, and delete blogs

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,70 @@
-const express = require('express')
+const express = require("express");
 const app = express();
- 
+const fs = require("fs");
 
-// YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+app.get("/blogs/:title", (req, res) => {
+  const { title } = req.params;
+
+  try {
+    if (fs.existsSync(title)) {
+      const content = fs.readFileSync(title, "utf-8");
+      res.send(content);
+    } else {
+      res.status(404).send("Blog not found");
+    }
+  } catch (err) {
+    res.status(500).send("Error reading file: " + err.message);
+  }
+});
+
+app.use(express.json());
+app.post("/blogs", function (req, res) {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res.status(400).send("Title and content are required");
+  }
+  try {
+    fs.writeFileSync(title, content);
+    res.status(201).send("Blog created successfully");
+  } catch (err) {
+    res.status(500).send("Error writing file: " + err.message);
+  }
+});
+
+app.put("/posts/:title", function (req, res) {
+  const { title } = req.params;
+  const { content } = req.body;
+  if (!content) {
+    return res.status(400).send("Content is required to update the blog");
+  }
+
+  try {
+    if (fs.existsSync(title)) {
+      fs.writeFileSync(title, content);
+      res.send("Blog updated successfully");
+    } else {
+      res.status(404).send("Blog not found");
+    }
+  } catch (err) {
+    res.status(500).send("Error updating file: " + err.message);
+  }
+});
+
+app.delete("/blogs/:title", (req, res) => {
+  const { title } = req.params;
+
+  try {
+    if (fs.existsSync(title)) {
+      fs.unlinkSync(title);
+      res.send("Blog deleted successfully");
+    } else {
+      res.status(404).send("Blog not found");
+    }
+  } catch (err) {
+    res.status(500).send("Error deleting file: " + err.message);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
