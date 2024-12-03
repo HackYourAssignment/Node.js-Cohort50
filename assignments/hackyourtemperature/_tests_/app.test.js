04721b66f01d616f7922cb349310624026513a95
Abdul Kader <abdulkader@tikapp.link>
2024-12-03 23:55:59 +0100

    Abdul Kader w2 node

---
+++ assignments/hackyourtemperature/_tests_/app.test.js
@@ -0,0 +1,31 @@
+import { app } from '../app.js';
+import supertest from 'supertest';
+
+const request = supertest(app);
+
+describe('POST /weather', () => {
+    it('should get 400 if cityName is empty', async () => {
+        const response = await request.post('/weather').send({});
+        expect(response.status).toBe(400);
+        expect(response.body).toEqual({
+            weatherText: 'City name is required!'
+        });
+    });
+
+    it('should get 404 if cityName is gibberish', async () => {
+        const response = await request
+            .post('/weather')
+            .send({ cityName: 'nonesence123' });
+        expect(response.status).toBe(404);
+        expect(response.body).toEqual({ weatherText: 'City is not found!' });
+    });
+
+    it('should get 200 if correct cityName is sent', async () => {
+        const response = await request
+            .post('/weather')
+            .send({ cityName: 'Den Haag' });
+
+        expect(response.status).toBe(200);
+        expect(response.body.weatherText).toContain('Den Haag');
+    });
+});
---
+++ assignments/hackyourtemperature/sources/keys.js
@@ -0,0 +1,4 @@
+const keys = {
+    API_KEY: '7fd63ddde6d16bb3769e4d483716e57a'
+  };
+  
