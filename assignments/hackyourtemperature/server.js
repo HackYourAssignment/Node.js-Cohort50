03bb5d02c4a9a55bf77fd78512b080c95163c047
Dalia-Saeed <dalisaeed09@gmail.com>
2024-12-04 10:05:00 +0100

    edit

--- assignments/hackyourtemperature/server.js
+++ assignments/hackyourtemperature/server.js
@@ -1,24 +1,21 @@
-// Import necessary modules
+
 import express from 'express';
 
 const app = express();
 const PORT = 3000;
 
-// Middleware to parse JSON data in POST requests
 app.use(express.json());
 
-// GET route for '/'
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
 
-// POST route for '/weather'
 app.post('/weather', (req, res) => {
-  const { cityName } = req.body; // Extract cityName from request body
+  const { cityName } = req.body; 
   res.send(`You submitted: ${cityName}`);
 });
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
